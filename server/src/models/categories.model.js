3eafeb8581180dd606084f951affdd811f198909
Christopher Owens <cowensdev@gmail.com>
2022-02-19 22:43:41 -0500

    Initial categories model setup

---
+++ server/src/models/categories.model.js
@@ -0,0 +1,22 @@
+const categories = [
+    {
+        id: 1,
+        title: 'Candles',
+    },
+    {
+        id: 2,
+        title: 'Wax Melts',
+    },
+    {
+        id: 3,
+        title: 'Seasonal',
+    },
+];
+
+function getAllCategories() {
+    return categories;
+}
+
+module.exports = {
+    getAllCategories,
+};
--- server/src/routes/categories/categories.controller.js
+++ server/src/routes/categories/categories.controller.js
@@ -1,5 +1,7 @@
+const { getAllCategories } = require('../../models/categories.model');
+
 function httpGetAllCategories(req, res) {
-    return res.status(200).json();
+    return res.status(200).json(getAllCategories());
 }
 
 function httpAddNewCategory(req, res) {
